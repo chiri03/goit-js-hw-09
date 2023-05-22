4ff0e5b3ad9a3ca1e8dde258406a4df7e8fdad84
chiri03 <lena.kiriac.97@gmail.com>
2023-05-22 22:40:11 +0300

    ex.9 task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startButton = document.querySelector('button[data-start]');
+const stoptButton = document.querySelector('button[data-stop]');
+
+let randomColorChange = null;
+
+startButton.addEventListener('click', () => {
+  randomColorChange = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startButton.disabled = true;
+  stoptButton.disabled = false;
+});
+stoptButton.addEventListener('click', () => {
+  clearInterval(randomColorChange);
+
+  stoptButton.disabled = true;
+  startButton.disabled = false;
+});
